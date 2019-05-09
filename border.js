7bd96ccf64d456161de2bfd8e9865fbb2616191e
Qiu <c.qiu@accenture.com>
2019-05-09 10:34:02 +0800

    v1.5.2

--- border.js
+++ border.js
@@ -54,46 +54,47 @@ function addBorder(type, color, defaultColor) {
 			color = defaultColor;
 		}
 	}
-
+	if (document.querySelector("#SalesforceProductionWarningLeftBar") != null) {
+		var leftBarObj = document.querySelector("#SalesforceProductionWarningLeftBar");
+		leftBarObj.parentNode.removeChild(leftBarObj);
+		var rightBarObj = document.querySelector("#SalesforceProductionWarningRightBar");
+		rightBarObj.parentNode.removeChild(rightBarObj);
+		var topBarObj = document.querySelector("#SalesforceProductionWarningTopBar");
+		topBarObj.parentNode.removeChild(topBarObj);
+		var buttomBarObj = document.querySelector("#SalesforceProductionWarningButtomBar");
+		buttomBarObj.parentNode.removeChild(buttomBarObj);
+	}
 	if (type === "lightning") {
-		if (document.querySelector("#SalesforceProductionWarningLeftBar") != null) {
-			var leftBarObj = document.querySelector("#SalesforceProductionWarningLeftBar");
-			leftBarObj.parentNode.removeChild(leftBarObj);
-			var rightBarObj = document.querySelector("#SalesforceProductionWarningRightBar");
-			rightBarObj.parentNode.removeChild(rightBarObj);
-			var topBarObj = document.querySelector("#SalesforceProductionWarningTopBar");
-			topBarObj.parentNode.removeChild(topBarObj);
-			var buttomBarObj = document.querySelector("#SalesforceProductionWarningButtomBar");
-			buttomBarObj.parentNode.removeChild(buttomBarObj);
-		}
 		var width = document.body.clientWidth > window.innerWidth ? window.innerWidth : document.body.clientWidth;
 		width = width - 5;
 		var height = window.innerHeight - 10;
-		var leftbar = document.createElement("DIV");
-		leftbar.id = "SalesforceProductionWarningLeftBar";
-		leftbar.style = "margin:0px;padding:0px;position: fixed;z-index: 100;width: 0px;height:" + height + "px;border:2.5px solid " + color + ";top:0px;left:0px;";
-
-		document.body.appendChild(leftbar);
-
-		var topbar = document.createElement("DIV");
-		topbar.id = "SalesforceProductionWarningTopBar";
-		topbar.style = "margin:0px;padding:0px;position: fixed;z-index: 100;width:" + width + "px;height:0px;border:2.5px solid " + color + ";top:0px;left:0px;";
-
-		document.body.appendChild(topbar);
-
-		var rightbar = document.createElement("DIV");
-		rightbar.id = "SalesforceProductionWarningRightBar";
-		rightbar.style = "margin:0px;padding:0px;position: fixed;z-index: 100;width: 0px;height:" + height + "px;border:2.5px solid " + color + ";top:0px;left:" + width + "px;";
-		document.body.appendChild(rightbar);
-
-		var buttombar = document.createElement("DIV");
-		buttombar.id = "SalesforceProductionWarningButtomBar";
-		buttombar.style = "margin:0px;padding:0px;position: fixed;z-index: 100;width:" + width + "px;height:0px;border:2.5px solid " + color + ";top:" + height + "px;left:0px;";
-		document.body.appendChild(buttombar);
 	} else {
-		document.body.style.border = "5px solid " + color;
+		var width = document.documentElement.clientWidth;
+		width = width - 5;
+		var height = document.documentElement.clientHeight;
+		height = height - 7;
 	}
+	var leftbar = document.createElement("DIV");
+	leftbar.id = "SalesforceProductionWarningLeftBar";
+	leftbar.style = "margin:0px;padding:0px;position: fixed;z-index: 100;width: 0px;height:" + height + "px;border:2.5px solid " + color + ";top:0px;left:0px;";
 
+	document.body.appendChild(leftbar);
+
+	var topbar = document.createElement("DIV");
+	topbar.id = "SalesforceProductionWarningTopBar";
+	topbar.style = "margin:0px;padding:0px;position: fixed;z-index: 100;width:" + width + "px;height:0px;border:2.5px solid " + color + ";top:0px;left:0px;";
+
+	document.body.appendChild(topbar);
+
+	var rightbar = document.createElement("DIV");
+	rightbar.id = "SalesforceProductionWarningRightBar";
+	rightbar.style = "margin:0px;padding:0px;position: fixed;z-index: 100;width: 0px;height:" + height + "px;border:2.5px solid " + color + ";top:0px;left:" + width + "px;";
+	document.body.appendChild(rightbar);
+
+	var buttombar = document.createElement("DIV");
+	buttombar.id = "SalesforceProductionWarningButtomBar";
+	buttombar.style = "margin:0px;padding:0px;position: fixed;z-index: 100;width:" + width + "px;height:0px;border:2.5px solid " + color + ";top:" + height + "px;left:0px;";
+	document.body.appendChild(buttombar);
 }
 function isProduction(s) {
 	var regu = /^(?!.*cs).(?!.*--).*\.lightning\.force\.com|(login\.|(ap|na|eu)[0-9]{1,3}\.|.*[^(cs)][^0-9]{1,3}\.my\.)(salesforce|visual\.force)\.com$/g;
